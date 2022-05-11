ce118b5be5486a62245dbe62f0b382cf345bbd0b
André Sterba <andre.sterba@stud.h-da.de>
2022-05-11 17:49:12 +0200

    Init

---
+++ README.md
@@ -0,0 +1,40 @@
+# go-ygot-generator-generator
+
+A tool used to configure the [ygot](https://github.com/openconfig/ygot#generating-go-structures-from-yang) generator via a simple yaml file.
+
+## Usage
+
+1. Create a `test.yaml` where you specify your ygot configuration.
+
+```yaml
+---
+generator_options:
+  - option: package_name
+    value: openconfig
+  - option: output_file
+    value: ./yang.go
+  - option: include_model_data
+    value: true
+  - option: generate_fakeroot
+    value: true
+  - option: fakeroot_name
+    value: device
+  - option: exclude_modules
+    value: ietf-interfaces
+models:
+  - ../../../models/openconfig/release/models/system/openconfig-system.yang
+  - ../../../models/openconfig/release/models/interfaces/openconfig-interfaces.yang
+  - ../../../models/openconfig/release/models/interfaces/openconfig-if-ethernet.yang
+  - ../../../models/openconfig/release/models/interfaces/openconfig-if-ip.yang
+path_to_generator: ../../../build-tools/generator
+path_to_models: ../../../models/openconfig
+```
+
+2. Let `go-ygot-generator-generator` generate to go file that can be used with `go generate`.
+
+```sh
+	go-yang-generator-generator test.yaml test.go
+	go generate test.go
+```
+
+
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+
+	"gopkg.in/yaml.v2"
+)
+
+var generatorTemplate = `package openconfig
+
+// This file is a placeholder in order to ensure that Go does not
+// find this directory to contain an empty package.
+
+//go:generate {{.PathToGenerator}} -path={{.PathToModels}} {{range .GeneratorOptions}} -{{.Option}}={{.Value}} {{end}} {{range .Models}} {{.}} {{end}}
+`
+
+type GeneratorOptions struct {
+	Option string `yaml:"option,omitempty"`
+	Value  string `yaml:"value,omitempty"`
+}
+
+type Manifest struct {
+	PackageName      string             `yaml:"package_name,omitempty"`
+	PathToGenerator  string             `yaml:"path_to_generator,omitempty"`
+	PathToModels     string             `yaml:"path_to_models,omitempty"`
+	GeneratorOptions []GeneratorOptions `yaml:"generator_options,omitempty"`
+	Models           []string           `yaml:"models,omitempty"`
+}
+
+func main() {
+	providedArgs := os.Args
+
+	if len(providedArgs) != 3 {
+		showHelp()
+	}
+
+	inputPath := providedArgs[1]
+	outputPath := providedArgs[2]
+
+	manifest, err := readManifest(inputPath)
+	if err != nil {
+		panic(err)
+	}
+
+	executeAndWriteManifest(outputPath, manifest)
+}
+
+func showHelp() {
+	fmt.Println(`Usage:
+go-yang-generator-generator <path-to-input-file> <path-to-output-file>`)
+
+	os.Exit(1)
+}
+
+func readManifest(path string) (*Manifest, error) {
+	config := Manifest{}
+
+	dat, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(dat, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	return &config, nil
+}
+
+func executeAndWriteManifest(path string, manifest *Manifest) error {
+	t1 := template.New("test")
+	t1parsed, err := t1.Parse(generatorTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+
+	err = t1parsed.Execute(f, &manifest)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
